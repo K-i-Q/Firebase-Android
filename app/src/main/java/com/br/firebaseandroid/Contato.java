c22615ec4bb1a987fdd170584f4ebdcf2b9e428c
unknown <Aluno@DEVE130341.educacional.up>
2019-10-16 20:27:02 -0300

    Criação do projeto

---
+++ app/src/main/java/com/br/firebaseandroid/Contato.java
@@ -0,0 +1,27 @@
+package com.br.firebaseandroid;
+
+public class Contato {
+    String nome;
+    String email;
+
+    public Contato(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ app/src/main/java/com/br/firebaseandroid/MainActivity.java
@@ -0,0 +1,31 @@
+package com.br.firebaseandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        Contato contato = new Contato("Juarez","juarez@gmail.com");
+
+        myRef.child("contatos").push().setValue(contato);
+
+
+    }
+}
